9a41a8ba5e40aa5c7f351f68bcaaf617f2f052a3
拔土豆的程序员 <lizhifuabc@163.com>
2023-09-04 14:39:30 +0800

    ‘项目创建3’

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  debug: true,
   lang: 'zh-CN',
   title: '拔土豆的程序员',
-  description: '不知道咋说，随便写写吧',
-
+  description: '不知道咋说，随便写写吧。',
+  // markdown设置
+  markdown:{
+    lineNumbers: true, // 代码块行号
+  },
   themeConfig: {
-    logo: '/public/images/logo.png',
+    // 添加导航栏
+    navbar: [
+      { text: '首页', link: '/' }
+    ],
   },
 }
